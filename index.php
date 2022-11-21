a5fcfccbc00e16788040b8304039036657b84a63
MrRowey <row.josh02@gmail.com>
2022-11-21 10:12:03 +0000

    pass eviroment code

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "{{ secrets.DB_SERVER}}";
-$username = "{{ secrets.DB_USERNAME}}";
-$password = "{{ secrets.DB_PASSWORD}}";
+$servername = "{{secrets.DB_SERVER}}";
+$username = "{{secrets.DB_USERNAME}}";
+$password = "{{secrets.DB_PASSWORD}}";
 
 $conn = mysqli_connect($servername, $username, $password);
 

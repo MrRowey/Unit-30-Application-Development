4cb96df1b0aa1c6838766a69a7dde932ed6444b7
Rowey <row.josh02@gmail.com>
2022-12-01 12:08:10 +0000

    add dbconfig

---
+++ dbConfig.php
@@ -0,0 +1,16 @@
+<?php
+ 
+$dbhost = "localhsot";
+$dbName = "nhs";
+$dbUser = "root";
+$dbpass = "";
+
+// create connection
+$conn = new mysqli($dbhost, $dbUser, $dbpass);
+
+// check connection
+if ($conn->connect_error){
+    die("Connection fail: " . $conn->connect_error);
+}
+echo "connected";
+?>
